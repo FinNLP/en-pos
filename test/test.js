5beaacfd51d82b9154102a4b78b902da1bc73a22
alexcorvi <alex@arrayy.com>
2017-01-26 07:33:22 +0300

    explain POS with '

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-const enpos = require("../lib/index.js");
+const pos = require("../lib/index.js");
 const wsj = require("./wsj.json");
-
 var tested = 0;
 var correct = 0;
 var wrong = 0;
 var diagnosis = [];
+
 
 console.log(" 	");
 console.log("	---------------------------");
@@ -13,10 +13,13 @@ console.log("	Starting Penn Treebank test");
 wsj.forEach((sample,index)=>{
 	if(index%1000 === 0) console.log("	",index);
 	if(index === wsj.length - 1) console.log("	",index);
-	enpos(sample.tokens).tags.forEach((answer,index,tags)=>{
+	pos(sample.tokens).tags.forEach((answer,index,tags)=>{
 		tested = tested + 1;
 		var expected = sample.tags[index];
 		if(answer === expected) correct = correct + 1;
+		else if(answer === "\"" && expected === "POS") correct = correct + 1; 	// this is because this POS tagger is intended to work with a special lexer
+																				// called "lexed" which doesn't pass the "'" POS tokens as merely "'"
+																				// it transforms and passes them as "'s"
 		else {
 			wrong = wrong + 1;
 			diagnosis.push({
