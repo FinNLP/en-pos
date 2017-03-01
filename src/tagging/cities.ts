0ab132080038b30be70a5cab4b2b00631508c6e9
alexcorvi <alex@arrayy.com>
2017-03-01 14:49:52 +0300

    solved node imports issue

--- src/tagging/cities.ts
+++ src/tagging/cities.ts
@@ -1,4 +1,7 @@
-import cities = require("cities-list");
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+
+const cities = require("cities-list");
+
 import capitalize from "./capitalize";
 
 export default function(token: string, sensitive?: boolean):string {
--- src/tagging/given_names.ts
+++ src/tagging/given_names.ts
@@ -1,5 +1,9 @@
-import names = require("humannames");
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+
+const names = require("humannames");
+
 import capitalize from "./capitalize";
+
 export default function(token:string,sensitive?:boolean):string{
 	if(!sensitive) token = capitalize(token);
 	if(names[token]) return "NNP";
