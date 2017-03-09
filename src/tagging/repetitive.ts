464f48efb8c63591da75aee281b12797125930f9
alexcorvi <alex@arrayy.com>
2017-03-09 15:54:26 +0300

    spellcheck

--- src/tagging/repetitive.ts
+++ src/tagging/repetitive.ts
@@ -2,19 +2,18 @@ import lexicon = require("en-lexicon");
 import slang from "./slang";
 
 function tryToken(token:string):string{
-	if(!countRepitions(token)) return "";
+	if(!countRepetitions(token)) return "";
 	token = token.toLowerCase();
-	token = removeRepition(token);
+	token = removeRepetition(token);
 	if(lexicon.lexicon[token]) return lexicon.lexicon[token].split("|")[0];
 	if(slang(token)) return slang(token);
 	return tryToken(token);
 }
 
-function countRepitions(token:string):number{
+function countRepetitions(token:string):number{
 	return token.split("").filter((a)=>token.indexOf(a)!==token.lastIndexOf(a)).length;
 }
-
-function removeRepition(token:string):string{
+function removeRepetition(token:string):string{
 	return token.replace(/(.)(?=\1)/,"");
 }
 
